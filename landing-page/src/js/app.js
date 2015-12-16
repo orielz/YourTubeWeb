8969bfa7e1b982557076e6b00f44883b39577b51
oriel zaken <orielu@gmail.com>
2015-12-16 18:30:39 +0200

    Arrow down + Windows icons

--- landing-page/src/js/app.js
+++ landing-page/src/js/app.js
@@ -1,24 +1,31 @@
 /**
  * Created by Oriel on 06/12/2015.
  */
-$(window).load(function() {
+$(window).load(function () {
     adjustBestAppearance();
+
+    $('.fa-arrow-circle-down').click(function () {
+        var $body = $('body');
+        var $redBg = $('div.full-bg');
+        var $logoRow = $('.logo-row');
+        $body.animate({scrollTop: ($redBg.height() + $logoRow.height())}, 500);
+    });
 });
 
-$( window ).resize(function() {
-    setTimeout(function() {
+$(window).resize(function () {
+    setTimeout(function () {
         adjustBestAppearance();
     }, 0);
 
 });
 
 function adjustBestAppearance() {
-    var viewPortHeight = $( window ).height();
-    var viewPortWidth = $( window ).width();
+    var viewPortHeight = $(window).height();
+    var viewPortWidth = $(window).width();
     var logoHeight = $('img.logo').parent().outerHeight();
 
     var fullBgHeight = viewPortHeight - logoHeight;
-    var imageHeight = fullBgHeight - 250;
+    var imageHeight = fullBgHeight - 280;
 
     $('div.full-bg').css({
         'min-height': viewPortHeight - logoHeight //,
